78eb0296663c35d0bcef16fd740cf0d6ee9245ec
Shyam jaiswal <jais.shyam@gmail.com>
2021-10-07 23:13:07 +0530

    revealed functions and boolean added, tokenURI changed for integrating bool reveal

--- contracts/PotionShop.sol
+++ contracts/PotionShop.sol
@@ -11,16 +11,20 @@ contract PotionShop is ERC721Enumerable, Ownable {
     // Starting and stopping sale and presale
     bool public saleActive = false;
     bool public presaleActive = false;
+    
+    //revealing the Metadata
+    bool public revealed = false;
 
 
     // Price of each token
     uint256 public price = 0.04 ether; // 1.yet to decide
 
     // Maximum limit of tokens that can ever exist
-    uint256 constant MAX_SUPPLY = 5; //2. needs correct number
+    uint256 constant MAX_SUPPLY = 10000;
 
     // The base link that leads to the image / video of the token
     string public baseTokenURI;
+    string public notRevealedUri;
 
     string public baseExtension = ".json";
 
@@ -30,8 +34,9 @@ contract PotionShop is ERC721Enumerable, Ownable {
     // List of addresses that have a number of reserved tokens for presale
     mapping (address => uint256) public presaleReserved;
 
-    constructor (string memory newBaseURI) ERC721 ("THE POTION SHOP", "TPS") {
+    constructor (string memory newBaseURI, string memory _initNotRevealedUri) ERC721 ("The Potion Shop", "TPS") {
         setBaseURI(newBaseURI);
+        setNotRevealedURI(_initNotRevealedUri);
     }
 
     // Override so the openzeppelin tokenURI() method will use this method to create the full tokenURI instead
@@ -80,22 +85,33 @@ contract PotionShop is ERC721Enumerable, Ownable {
     // Edit reserved presale spots
     function editPresaleReserved(address[] memory _a) public onlyOwner {
         for(uint256 i; i < _a.length; i++){
-            presaleReserved[_a[i]] = 2; /// Only 2 reserved per account
+            presaleReserved[_a[i]] = 2; // Only 2 reserved per account
         }
     }
     
     // override to ensure correct tokenURI
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+      {
         require(
-        _exists(tokenId),
-        "ERC721Metadata: URI query for nonexistent token"
+          _exists(tokenId),
+          "ERC721Metadata: URI query for nonexistent token"
         );
-
+        
+        if(revealed == false) {
+            return notRevealedUri;
+        }
+    
         string memory currentBaseURI = _baseURI();
         return bytes(currentBaseURI).length > 0
             ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
             : "";
-    }
+      }
+
 
     // Start and stop presale
     function setPresaleActive(bool val) public onlyOwner {
@@ -110,6 +126,14 @@ contract PotionShop is ERC721Enumerable, Ownable {
     // Set new baseURI
     function setBaseURI(string memory baseURI) public onlyOwner {
         baseTokenURI = baseURI;
+    }
+    
+    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+        notRevealedUri = _notRevealedURI;
+    }
+    
+    function reveal() public onlyOwner() {
+      revealed = true;
     }
 
     // Set a different price in case ETH changes drastically
